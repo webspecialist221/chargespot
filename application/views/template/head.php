cf3d063034652e5e7303763d7a749fa69bd8dfd8
webspecialist221 <webspecialist221@gmail.com>
2016-08-05 15:34:27 +0500

    1st upload

---
+++ application/views/template/head.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<base href="<?= base_url();?>">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <!-- Meta, title, CSS, favicons, etc. -->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Gentallela Alela! | </title>
+
+  <!-- Bootstrap core CSS -->
+
+  <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+
+  <link href="assets/fonts/css/font-awesome.min.css" rel="stylesheet">
+  <link href="assets/css/animate.min.css" rel="stylesheet">
+
+  <!-- Custom styling plus plugins -->
+  <link href="assets/css/custom.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="assets/css/maps/jquery-jvectormap-2.0.3.css" />
+  <link href="assets/css/icheck/flat/green.css" rel="stylesheet" />
+  <link href="assets/css/floatexamples.css" rel="stylesheet" type="text/css" />
+
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/js/nprogress.js"></script>
+
+  <!--[if lt IE 9]>
+        <script src="../assets/js/ie8-responsive-file-warning.js"></script>
+        <![endif]-->
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+</head>
