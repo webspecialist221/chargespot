a0a309c783f13b1aa23c20b9201fb14c46a6727c
webspecialist221 <webspecialist221@gmail.com>
2016-08-06 11:32:39 +0500

    change image url

--- application/views/template/sidebar.php
+++ application/views/template/sidebar.php
@@ -9,7 +9,7 @@
           <!-- menu prile quick info -->
           <div class="profile">
             <div class="profile_pic">
-              <img src="images/img.jpg" alt="..." class="img-circle profile_img">
+              <img src="assets/images/img.jpg" alt="..." class="img-circle profile_img">
             </div>
             <div class="profile_info">
               <span>Welcome,</span>
